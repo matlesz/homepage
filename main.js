3232d1c1b6d6f062ec4c9f72b983bf563bf5afac
matlesz <matlesz@gmail.com>
2019-06-15 13:30:20 +0200

    add DOM selecotr

--- main.js
+++ main.js
@@ -3,4 +3,17 @@ const name = "Mateusz";
 const age = 29;
 console.log(name);
 console.log(age);
+
 console.log(`Nazywam sie ${name} i mam ${age} lat🦁`);
+
+
+const about = document.querySelector('.about__paragraph--js');
+
+console.log(about.innerHTML);
+
+about.innerHTML = `<strong>Nazywam sie </strong> ${name} i mam ${age} lat🦁`
+
+const paragaphs = document.querySelectorAll('p');
+
+console.log(paragaphs);
+console.log(paragaphs[1]);
